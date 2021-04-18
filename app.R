947a34ab48edb42b346d700238cd6ff5b6f18b6d
benedictorenes <benedictorenes@hotmail.com>
2021-04-18 16:10:45 +0200

    Add a plot output for the humidity. Now, the number of points to show in the plot is defined at the beginning of app.R, and it is default to 10

--- app.R
+++ app.R
@@ -12,8 +12,7 @@ library(scales)
 
 # file name to store serial print data
 fname = "test/serial_data2.txt"
-
-#datiles = read.table(fname,header = F,sep = "\t",col.names = c("Time","Temperature","Humidity"),as.is = T)
+N = 20 # number of data points to show in the plot
 
 # ui: 
 #   - Output plot 
@@ -42,6 +41,7 @@ ui <- fluidPage(
         # Output plot
         mainPanel(
            plotOutput(outputId = "LogPlot"),
+           plotOutput(outputId = "LogPlot2"),
            width = 7
         )
     )
@@ -58,8 +58,8 @@ server <- function(input, output) {
     #handy: reactive values ymin and ymax for updating plot limits. They will be wrapped
     #   around reactives() lower_ylim, higher_ylim
     handy = reactiveValues(ymin = 10, ymax = 30)
-    lower_ylim = reactive({handy$ymin = min(tail(myvals$log22$Temperature,10),na.rm = T)-5})
-    higher_ylim = reactive({handy$ymax = max(tail(myvals$log22$Temperature,10),na.rm = T)+5})
+    lower_ylim = reactive({handy$ymin = min(tail(myvals$log22$Temperature,N),na.rm = T)-5})
+    higher_ylim = reactive({handy$ymax = max(tail(myvals$log22$Temperature,N),na.rm = T)+5})
     
     
     # fileData() will update every time the log file has been modified
@@ -96,7 +96,7 @@ server <- function(input, output) {
     # Plot showing the last 10 values of the temperature in the log22 data frame
     output$LogPlot <- renderPlot({
         # draw the plot
-        ggplot(data = tail(myvals$log22,10), aes(x=as.POSIXct(strptime(Time,format = "%Y-%m-%d %H:%M:%S")), y=Temperature)) +
+        ggplot(data = tail(myvals$log22,N), aes(x=as.POSIXct(strptime(Time,format = "%Y-%m-%d %H:%M:%S")), y=Temperature)) +
             geom_point(col = 2, pch = 18, size = 3) + 
             geom_path()+
             theme_minimal() + 
@@ -107,6 +107,22 @@ server <- function(input, output) {
             scale_x_datetime(labels = date_format("%Y-%m-%d %H:%M:%S")) +
             ylim(lower_ylim(),higher_ylim())
     })
+    
+    ## second plot for the Humidity
+    output$LogPlot2 <- renderPlot({
+        # draw the plot
+        ggplot(data = tail(myvals$log22,N), aes(x=as.POSIXct(strptime(Time,format = "%Y-%m-%d %H:%M:%S")), y=Humidity)) +
+            geom_point(col = 2, pch = 18, size = 3) + 
+            geom_path()+
+            theme_minimal() + 
+            theme(plot.title = element_text(size=10)) + 
+            ggtitle("Lab Humidity") +
+            ylab("Rel. Humidity (%)") +
+            xlab("Time stamp") +
+            scale_x_datetime(labels = date_format("%Y-%m-%d %H:%M:%S")) +
+            ylim(15,80)
+    })
+    
     
     # when clicking on the save Log button, write a file with the logged data
     observeEvent(input$saveButton,{
---
+++ secondary_axis_plot_test.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(ggplot2)
+library(scales)
+library(gridExtra)
+
+fname = "log/sensor_log.csv"
+datiles = read.table(fname,header = T,sep = ",")
+
+
+
+p1 = ggplot(data = tail(datiles,10), aes(x=as.POSIXct(strptime(Time,format = "%Y-%m-%d %H:%M:%S")))) +
+  geom_point(aes(y=Temperature),col = 2, pch = 18, size = 3) + 
+  geom_path(aes(y=Temperature))+
+  geom_point(aes(y=Humidity/4),col = 3, pch = 16, size = 2) + 
+  geom_path(aes(y=Humidity/4),col = 3)+
+  scale_x_datetime(labels = date_format("%Y-%m-%d %H:%M:%S")) +
+  theme_minimal()  + 
+  theme(plot.title = element_text(size=10)) +
+  ggtitle("Lab temperature") +
+  ylab("Temperature (celsius)") +
+  xlab("Time stamp") +
+  scale_y_continuous(sec.axis = sec_axis(trans=~.*2., name="Rel. Humidity (5)")) +
+  theme(axis.title.y = element_text(color = 2, size=10),
+        axis.title.y.right = element_text(color = 3, size=10)) 
+
+
+p2 = ggplot(data = tail(datiles,10), aes(x=as.POSIXct(strptime(Time,format = "%Y-%m-%d %H:%M:%S")))) +
+  geom_point(aes(y=Temperature),col = 2, pch = 18, size = 3) + 
+  geom_path(aes(y=Temperature))+
+  scale_x_datetime(labels = date_format("%Y-%m-%d %H:%M:%S")) +
+  theme_minimal()  + 
+  theme(plot.title = element_text(size=10)) +
+  ggtitle("Lab temperature") +
+  ylab("Temperature (celsius)") +
+  xlab("Time stamp") 
+
+
+
+grid.arrange(p1,p2,ncol = 2)
